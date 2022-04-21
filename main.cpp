352c440a1ab36a7154c16c87f2cf569a4fd4c148
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2022-04-22 00:15:46 +0300

    Artificial basis method implemented

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ struct constraint_t
 {
 	weights_t weights;
 	upper_limit_t limit;
+	int direction = 1;
 };
 
 using values_t = std::vector<double>;
@@ -28,65 +29,49 @@ double evaluate(const weights_t& coeffs, const values_t& values)
 	return sum;
 }
 
-values_t _maximizing_simplex_method_phase2(const std::vector<constraint_t>& constraints, const weights_t& objective)
+values_t minimizing_simplex_method(const std::vector<constraint_t>& constraints, const weights_t& objective)
 {
 	//vars:
-	//[0; N-1] = in/out vars
-	//[N; N+M-1] = dummy vars
-	//[N+M] = objective
+	//[0; N-1] = vars
+	//[N] = objective
+	//[N+1; N+M] = dummy vars
 
 	//tableau:
-	//         (IO vars) (dummy vars) (objective) | RHS
-	// eq1
-	// ...
-	// eqM
+	//            (vars) (obj) (dummy vars) [obj2] (artificial vars) | RHS
+	//(ineqs)
+	//(obj)
+	//[obj2]
+
+	//where obj2 is an artificial objective for the purpose of canonnicalizing
+
 
 	static constexpr double error_tolerance = 1e-8;
 
 	using row_t = std::vector<double>;
 	using matrix_t = std::vector<row_t>;
 
-	const size_t N = objective.size(); //IO vars
-	const size_t M = constraints.size(); //dummy vars
+	size_t n_vars = objective.size();
+	size_t n_constraints = constraints.size();
+	size_t n_equational_vars = n_constraints;
+	size_t n_artificial_vars = n_constraints;
 
 	for (const auto& constraint : constraints)
-		assert(constraint.weights.size() == N);
+		assert(constraint.weights.size() == n_vars);
 
-	const size_t width = N + M + 2;
-	const size_t vars_count = N + M;
-	const size_t height = M + 1;
-
+	size_t total_vars_count = n_vars + n_equational_vars + 1 + n_artificial_vars;
+	size_t width = total_vars_count + 2;
+	size_t height = n_constraints + 2;
+	size_t objective_column = width - 1;
 
 
 	std::vector<size_t> row_associated_vars;
-	row_associated_vars.resize(M);
-	for (size_t i = 0; i < M; ++i)
-		row_associated_vars[i] = i + N;
+	row_associated_vars.resize(height, -1);
 
 
 
 	matrix_t tableau;
 	tableau.resize(height);
-	
-	for (size_t i = 0; i < M; ++i)
-	{
-		tableau[i].resize(width);
-		for (size_t j = 0; j < N; ++j)
-			tableau[i][j] = constraints[i].weights[j];
-		for (size_t j = N; j < N + M; ++j)
-			tableau[i][j] = double(i == (j - N));
-		tableau[i][N + M] = 0;
-		tableau[i][N + M + 1] = constraints[i].limit;
-	}
 
-	tableau[M].resize(width);
-	for (size_t j = 0; j < N; ++j)
-		tableau[M][j] = -objective[j];
-	for (size_t j = N; j < N + M; ++j)
-		tableau[M][j] = 0;
-	tableau[M][N + M] = 1;
-	tableau[M][N + M + 1] = 0;
-	
 
 
 	auto get_simplex_ratio = [&]
@@ -94,18 +79,24 @@ values_t _maximizing_simplex_method_phase2(const std::vector<constraint_t>& cons
 	{
 		if (tableau[i][j] <= 0)
 			return INFINITY;
-		return  tableau[i][N + M + 1] / tableau[i][j];
+		return  tableau[i][width - 1] / tableau[i][j];
 	};
 
+	auto add_row = [&]
+	(size_t to, size_t which, double factor)
+	{
+		for (size_t j = 0; j < width; ++j)
+			tableau[to][j] += tableau[which][j] * factor;
+	};
 
 	auto try_rowwise_transformation = [&]
-	(size_t min_column) -> bool
+	(size_t pivot_column) -> bool
 	{
 		size_t min_row = 0;
-		double min_ratio = get_simplex_ratio(0, min_column);
-		for (size_t i = 1; i < M; ++i)
+		double min_ratio = get_simplex_ratio(min_row, pivot_column);
+		for (size_t i = 1; i < n_constraints; ++i)
 		{
-			double current_ratio = get_simplex_ratio(i, min_column);
+			double current_ratio = get_simplex_ratio(i, pivot_column);
 			if (current_ratio < min_ratio)
 			{
 				min_ratio = current_ratio;
@@ -117,85 +108,131 @@ values_t _maximizing_simplex_method_phase2(const std::vector<constraint_t>& cons
 			return false;
 
 		const size_t old_basis = row_associated_vars[min_row];
-		if (old_basis == min_column)
+		if (old_basis == pivot_column)
 			return false;
 
-		const double intersection_elem = tableau[min_row][min_column];
+		const double intersection_elem = tableau[min_row][pivot_column];
 		for (size_t j = 0; j < width; ++j)
 			tableau[min_row][j] /= intersection_elem;
 
-		for (size_t i = 0; i <= M; ++i)
+		for (size_t i = 0; i < height; ++i)
 		{
 			if (i == min_row)
 				continue;
 
-			const double row_factor = tableau[i][min_column];
-			for (size_t j = 0; j < width; ++j)
-				tableau[i][j] -= tableau[min_row][j] * row_factor;
+			add_row(i, min_row, -tableau[i][pivot_column]);
 		}
 
-		row_associated_vars[min_row] = min_column;
+		row_associated_vars[min_row] = pivot_column;
 		return true;
 	};
 
-	size_t iters = vars_count * vars_count + 1;
-	while (iters --> 0)
+	auto pick_pivoting_column = [&]() -> size_t
 	{
-		auto& objective_row = tableau[M];
-		size_t min_column = 0;
-		for (size_t i = 1; i < vars_count; ++i)
+		size_t pivot_column = 0;
+		auto& objective_row = tableau[height - 1];
+		for (size_t i = 1; i < total_vars_count; ++i)
 		{
-			if (objective_row[i] < objective_row[min_column])
-				min_column = i;
+			if (i == objective_column)
+				continue;
+			if (objective_row[i] > objective_row[pivot_column])
+				pivot_column = i;
 		}
+		return pivot_column;
+	};
 
-		if (objective_row[min_column] > error_tolerance)
-			break;
-
-		if (objective_row[min_column] >= -error_tolerance)
+	auto solve_current_objective = [&]
+	{
+		auto& vars = row_associated_vars;
+		size_t iters = total_vars_count * total_vars_count;
+		while (iters-- > 0)
 		{
-			break;
+			auto& objective_row = tableau[height - 1];
+			size_t pivot_column = pick_pivoting_column();
+
+			if (objective_row[pivot_column] <= error_tolerance)
+				break;
+
+			if (!try_rowwise_transformation(pivot_column))
+				break;
 		}
-			//got zero as best choice, try finding at least something
-		//	static std::vector<size_t> idx;
-		//	idx.clear();
-		//	idx.resize(vars_count);
-		//	for (size_t i = 0; i < vars_count; ++i)
-		//		idx[i] = i;
+	};
 
-		//	bool ok = false;
-		//	while (!idx.empty())
-		//	{
-		//		const size_t n = rand() % idx.size();
-		//		const size_t j = idx[n];
-		//		idx.erase(idx.begin() + n);
 
-		//		if (objective_row[j] > error_tolerance)
-		//			continue;
-		//		if (true == (ok = try_rowwise_transformation(j)))
-		//			break;
-		//	}
-		//	if (!ok)
-		//		break;
-		//}
-		//else
-		//{
-		//	if (!try_rowwise_transformation(min_column))
-		//		break;
-		//}
 
+	//Phase 1: cannonicalizing
+
+	//Constraints data
+	for (size_t i = 0; i < n_constraints; ++i)
+	{
+		tableau[i].resize(width);
+
+		for (size_t j = 0; j < n_vars; ++j)
+			tableau[i][j] = constraints[i].weights[j];
+		tableau[i][width - 1] = constraints[i].limit;
+
+		tableau[i][n_vars + 1 + i] = constraints[i].direction;
+
+		if (constraints[i].limit < 0)
+			add_row(i, i, -2); //negate the row
+
+		tableau[i][n_vars + 1 + n_equational_vars + 1 + i] = 1;
+	}
+
+
+	//Original objective row
+	tableau[height - 2].resize(width);
+	for (size_t j = 0; j < n_vars; ++j)
+		tableau[height - 2][j] = -objective[j];
+	tableau[height - 2][n_vars] = 1;
+
+
+	//Artificial objective row
+	tableau[height - 1].resize(width);
+	for (size_t j = 0; j < n_equational_vars; ++j)
+		tableau[height - 1][n_vars + 1 + n_equational_vars + 1 + j] = -1;
+	tableau[height - 1][n_vars + 1 + n_equational_vars] = 1;
+
+
+
+	for (size_t i = 0; i < n_constraints; ++i)
+		add_row(height - 1, i, 1);
+
+
+
+	//Phase 1: cannonicalizing
+	solve_current_objective();
+
+	if (fabs(tableau[height - 1][width - 1]) > error_tolerance)
+		return {};
+
+	--height;
+	width -= n_artificial_vars + 1;
+	total_vars_count = n_vars + n_equational_vars;
+	objective_column = n_vars;
+
+	tableau.pop_back();
+	for (auto& row : tableau)
+	{
+		row[n_vars + 1 + n_equational_vars] = row.back();
+		row.resize(width);
 	}
 
 
 
-	values_t result(N, 0);
-	for (size_t i = 0; i < N; ++i)
+	//Phase 2: solving objective
+	solve_current_objective();
+
+
+
+	values_t result(n_vars, 0);
+	for (size_t i = 0; i < n_vars; ++i)
 	{
-		for (size_t j = 0; j < M; ++j)
+		for (size_t j = 0; j < n_constraints; ++j)
 		{
 			if (row_associated_vars[j] == i)
 			{
-				result[i] = tableau[j][N + M + 1];
+				result[i] = tableau[j][width - 1];
 				break;
 			}
 		}
@@ -211,31 +248,21 @@ values_t _maximizing_simplex_method_phase2(const std::vector<constraint_t>& cons
 			return {};
 	}
 
+	result.push_back(tableau[height - 1][width - 1]);
 	return result;
 }
 
-
-values_t maximizing_simplex_method(std::vector<constraint_t> constraints, const weights_t& objective)
+values_t maximizing_simplex_method(const std::vector<constraint_t>& constraints, weights_t objective)
 {
+	for (auto& x : objective)
+		x = -x;
 
+	return minimizing_simplex_method(constraints, objective);
 }
-
-values_t minimizing_simplex_method(std::vector<constraint_t> constraints, weights_t objective)
-{
-	//for (auto& x : objective)
-	//	x = -x;
-
-	//const size_t n = objective.size();
-	//weights_t tmp_obj(n * 2);
-	//for (size_t i = 0; i < n; ++i)
-	//	tmp_obj[i + n] = 1;
-
-	//return _maximizing_simplex_method_phase2(constraints, objective);
-}
-values_t minimizing_integer_simplex_method(const std::vector<constraint_t>& constraints, const weights_t& objective)
-{
-	return {};
-}
+//values_t minimizing_integer_simplex_method(const std::vector<constraint_t>& constraints, const weights_t& objective)
+//{
+	//return {};
+//}
 
 
 
@@ -243,9 +270,9 @@ values_t minimizing_integer_simplex_method(const std::vector<constraint_t>& cons
 int main()
 {
 	const std::vector<constraint_t> constraints = {
-		{{1, 0}, 3},
-		{{-1, 1}, 1},
-		{{-1, 0}, -1},
+		{{1, 0}, 3, 1},
+		{{-1, 1}, 1, 1},
+		{{1, 0}, 1, -1},
 	};
 
 	const weights_t objective{-2, 1};
